c9dcdbc6e5bc2c1b854b3950bafa846f1b9aa777
Chris KIM <codewalker@hardkernel.com>
2019-06-10 14:38:10 +0900

    ODROID-C4: Fix to unify defien about model name.
    Change-Id: I1f32468981688677173db5f760686c857251a8cb

--- Android.mk
+++ Android.mk
@@ -26,7 +26,7 @@ ifeq ($(TARGET_BUILD_VARIANT), user)
 LOCAL_CFLAGS += -DHWC_RELEASE=1
 endif
 
-ifeq ($(TARGET_BOARD_PLATFORM), odroidc4)
+ifeq ($(PRODUCT_BRAND), ODROID)
 LOCAL_CFLAGS += -DHWC_RELEASE=1
 endif
 
@@ -129,7 +129,7 @@ LOCAL_CFLAGS += -DHWC_ENABLE_DISPLAY_MODE_MANAGEMENT
 endif
 #*********************************HWC CONFIGS END************************
 
-ifeq ($(TARGET_PRODUCT), odroidc4)
+ifeq ($(PRODUCT_BRAND), ODROID)
 LOCAL_CFLAGS += -DODROID
 endif
 
